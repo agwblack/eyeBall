2f5ff9b39b0f69f242636392dbb0459c53628193
Andrew Black <agwblack@gmail.com>
2012-05-27 21:54:46 +0100

    Added new Types.h file and some notes on how to handle relative movement.

---
+++ eyeBall/Classes/Types.h
@@ -0,0 +1,14 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+/* *
+ * Cartesian measures a signed magnitude in both the horizontal and vertical
+ * plane
+ * */
+typedef struct 
+{
+  double x;
+  double y;
+} Cartesian;
+
+#endif
